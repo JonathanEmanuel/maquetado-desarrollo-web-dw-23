0d20091f1eb7eac2649d6939d0dd44866e148db6
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-06-06 21:15:18 -0300

    Clase 13 - transition y animation

--- Clase_13/script.js
+++ Clase_13/script.js
@@ -2,3 +2,11 @@ document.querySelector('.menu-mobile i').addEventListener('click', function(){
     document.querySelector('nav > ul').classList.toggle('visible')    
 })
 
+const imagenes = document.querySelectorAll('.img-lenguajes');
+console.log(imagenes)
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', function(){
+        imagen.classList.toggle('animacion')
+    })
+});
